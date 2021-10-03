05139408b08489c8130a40585fa652fb40d538cb
an-dyy <andy.development@protonmail.com>
2021-10-03 17:31:17 -0400

    feat: Semi impl interaction *ish, idk man*

--- lefi/client.py
+++ lefi/client.py
@@ -9,6 +9,8 @@ from .http import HTTPClient
 from .state import State
 from .ws import WebSocketClient
 
+from .interactions import App
+
 if typing.TYPE_CHECKING:
     from .objects import Message
 
@@ -17,16 +19,22 @@ __all__ = ("Client",)
 
 class Client:
     def __init__(
-        self, token: str, loop: typing.Optional[asyncio.AbstractEventLoop] = None
+        self,
+        token: str,
+        loop: typing.Optional[asyncio.AbstractEventLoop] = None,
+        pub_key: typing.Optional[str] = None,
     ):
         self.loop: asyncio.AbstractEventLoop = loop or asyncio.get_event_loop()
         self.http: HTTPClient = HTTPClient(token, self.loop)
         self._state: State = State(self, self.loop)
         self.ws: WebSocketClient = WebSocketClient(self)
+        self.pub_key: typing.Optional[str] = pub_key
 
         self.events: typing.Dict[str, typing.List[typing.Callable]] = {}
 
-    def add_listener(self, func: typing.Callable, event_name: typing.Optional[str]) -> None:
+    def add_listener(
+        self, func: typing.Callable, event_name: typing.Optional[str]
+    ) -> None:
         name = event_name or func.__name__
         if not inspect.iscoroutinefunction(func):
             raise TypeError("Callback must be a coroutine")
@@ -43,6 +51,11 @@ class Client:
 
         return inner
 
+    async def _init_app(self) -> None:
+        if self.pub_key is not None:
+            self.http_server = App(self, self.pub_key)
+            await self.http_server.run()
+
     async def connect(self) -> None:
         await self.ws.start()
 
@@ -50,7 +63,9 @@ class Client:
         await self.http.login(token)
 
     async def start(self) -> None:
-        await asyncio.gather(self.login(self.http.token), self.connect())
+        await asyncio.gather(
+            self._init_app(), self.login(self.http.token), self.connect()
+        )
 
     def get_message(self, id: int) -> typing.Optional[Message]:
         return self._state._messages.get(id)
