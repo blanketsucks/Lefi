c03132f734085dcdf11fe0de5fc9c2e7f0765720
an-dyy <andy.development@protonmail.com>
2021-10-07 21:21:55 -0400

    docs: Semi documented `http.py` I will finish tomorrow, my sanity is dwindling from writing docstrings.

--- lefi/http.py
+++ lefi/http.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import aiohttp
 import asyncio
 
-from typing import ClassVar, List, Dict, Any
+from typing import ClassVar, List, Dict, Any, Optional
 
 from .utils import update_payload
 from .errors import HTTPException, Forbidden, NotFound, BadRequest, Unauthorized
@@ -14,6 +14,19 @@ BASE: str = "https://discord.com/api/v9"
 
 
 class HTTPClient:
+    """
+    A class used to send and handle requests to the discord API.
+
+    Attributes:
+        token (str): The clients token, used for authorization.
+        loop (asyncio.AbstractEventLoop): The [asyncio.AbstractEventLoop][] being used.
+        session (aiohttp.ClientSession): The [aiohttp.ClientSession][] to use for sending requests.
+
+    Note:
+        This class is used behind the scenes, this is not intended to be called directly.
+
+    """
+
     ERRORS: ClassVar[Dict[int, Any]] = {
         400: BadRequest,
         401: Unauthorized,
@@ -22,14 +35,47 @@ class HTTPClient:
     }
 
     def __init__(self, token: str, loop: asyncio.AbstractEventLoop) -> None:
+        """
+        Parameters:
+            token (str): The token to use for authorzation.
+            loop (asyncio.AbstractEventLoop): The [asyncio.AbstractEventLoop][] to use.
+            session (aiohttp.ClientSession): The [aiohttp.ClientSession][] to use for sending requests.
+
+        """
         self.token: str = token
         self.loop: asyncio.AbstractEventLoop = loop
         self.session: aiohttp.ClientSession = None  # type: ignore
 
     async def _create_session(self, loop: asyncio.AbstractEventLoop = None) -> aiohttp.ClientSession:
+        """
+        Creates the session to use if one wasn't given during construction.
+
+        Parameters:
+            loop (asyncio.AbstractEventLoop): The [asyncio.AbstractEventLoop][] to use for the session.
+
+        Returns:
+            The created [aiohttp.ClientSession][] instance.
+
+        """
         return aiohttp.ClientSession(loop=self.loop or loop, headers={"Authorization": f"Bot {self.token}"})
 
     async def request(self, method: str, path: str, **kwargs) -> Any:
+        """
+        Makes a request to the discord API.
+
+        Parameters:
+            method (str): The method for the request.
+            path (str): The endpoint which to send the request to.
+            **kwargs (Any): Any extra options to pass to [aiohttp.ClientSession.request][]
+
+        Returns:
+            The data returned from the request.
+
+        Raises:
+            [lefi.errors.HTTPException][] if any error was received from the request.
+
+        """
+
         if self.session is None or self.session.closed:
             self.session = await self._create_session()
 
@@ -54,34 +100,79 @@ class HTTPClient:
             raise error(data)
 
     async def get_bot_gateway(self) -> Dict:
+        """
+        A method which gets the gateway url.
+
+        Returns:
+            A dict which should contain the url.
+
+        """
         return await self.request("GET", "/gateway/bot")
 
     async def ws_connect(self, url: str) -> aiohttp.ClientWebSocketResponse:
+        """
+        A method which attempts to connect to the websocket.
+
+        Returns:
+            A [aiohttp.ClientWebSocketResponse][] instance.
+
+        """
         return await self.session.ws_connect(url)
 
     async def login(self) -> None:
+        """
+        Checks to see if the token given is valid.
+
+        Raises:
+            ValueError if an invalid token was passed.
+
+        """
         try:
             await self.request("GET", "/users/@me")
         except (Forbidden, Unauthorized):
             raise ValueError("Invalid token")
 
     async def get_channel(self, channel_id: int) -> Dict[str, Any]:
+        """
+        Makes an API call to get a channel.
+
+        Parameters:
+            channel_id (int): The channel's ID.
+
+        Returns:
+            A dict representing the channel.
+
+        """
         return await self.request("GET", f"/channels/{channel_id}")
 
     async def edit_text_channel(
         self,
         channel_id: int,
         *,
-        name: str = None,
-        type: int = None,
-        position: int = None,
-        topic: str = None,
-        nsfw: bool = None,
-        rate_limit_per_user: int = None,
-        permission_overwrites: List[Dict[str, Any]] = None,
-        parent_id: int = None,
-        default_auto_archive_duration: int = None,
+        name: Optional[str] = None,
+        type: Optional[int] = None,
+        position: Optional[int] = None,
+        topic: Optional[str] = None,
+        nsfw: Optional[bool] = None,
+        rate_limit_per_user: Optional[int] = None,
+        permission_overwrites: Optional[List[Dict[str, Any]]] = None,
+        default_auto_archive_duration: Optional[int] = None,
     ) -> Dict[str, Any]:
+        """
+        Makes an API call to edit a text channel.
+
+        Parameters:
+            channel_id (int): The channel id representing the channel to edit.
+            name (Optional[str]): The new name for the channel.
+            type (Optional[int]): The new type for the channel.
+            position (Optional[int]): The new position for the channel.
+            topic (Optional[str]): The new topic for the channel.
+            nsfw (Optional[bool]): Whether or not the channel should be NSFW.
+            rate_limit_per_user (Optional[int]): The new slowmode of the channel.
+            permissions_overwrites (Optional[List[Dict[str, Any]]]): The new permission overwrites for the channel.
+            default_auto_archive_duration (Optional[List[Dict[str, Any]]]): New time for threads to auto archive.
+
+        """
         payload = update_payload(
             {},
             name=name,
@@ -91,10 +182,8 @@ class HTTPClient:
             nsfw=nsfw,
             rate_limit_per_user=rate_limit_per_user,
             permission_overwrites=permission_overwrites,
-            parent_id=parent_id,
             default_auto_archive_duration=default_auto_archive_duration,
         )
-
         return await self.request("PATCH", f"/channels/{channel_id}", json=payload)
 
     async def edit_voice_channel(
