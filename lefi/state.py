4c6f54d8dec660d4242913163768f47c077344e3
an-dyy <andy.development@protonmail.com>
2021-10-09 10:24:27 -0400

    docs: Added documentation for `state.py`

--- lefi/state.py
+++ lefi/state.py
@@ -31,7 +31,20 @@ T = TypeVar("T")
 
 
 class Cache(collections.OrderedDict[Union[str, int], T]):
+    """
+    A class which acts as a cache for objects.
+
+    Attributes:
+        maxlen (Optional[int]): The max amount the cache can hold.
+
+    """
+
     def __init__(self, maxlen: Optional[int] = None, *args, **kwargs):
+        """
+        Parameters:
+            maxlen (Optional[int]): The max amount the cache can hold.
+
+        """
         super().__init__(*args, **kwargs)
         self.maxlen: Optional[int] = maxlen
         self._max: int = 0
@@ -48,6 +61,19 @@ class Cache(collections.OrderedDict[Union[str, int], T]):
 
 
 class State:
+    """
+    A class which represents the connection state between the client and discord.
+
+    Attributes:
+        client (lefi.Client): The [lefi.Client][] instance being used.
+        loop (asyncio.AbstractEventLoop): The [asyncio.AbstractEventLoop][] being used.
+        http (lefi.HTTPClient): The [lefi.HTTPClient][] handling requests
+
+    Note:
+        This class is used behind the scenes. **It is not meant to called directly**
+
+    """
+
     CHANNEL_MAPPING: Dict[
         int,
         Union[
@@ -65,6 +91,12 @@ class State:
     }
 
     def __init__(self, client: Client, loop: asyncio.AbstractEventLoop):
+        """
+        Parameters:
+            client (lefi.Client): The client being used.
+            loop (asyncio.AbstractEventLoop): The [asyncio.AbstractEventLoop][] being used.
+
+        """
         self.client = client
         self.loop = loop
         self.http = client.http
@@ -77,6 +109,14 @@ class State:
         ]()
 
     def dispatch(self, event: str, *payload: Any) -> None:
+        """
+        Dispatches data to callbacks registered to events after parsing is finished.
+
+        Parameters:
+            event (str): The name of the event to dispatch to.
+            *payload (Any): The data after parsing is finished.
+
+        """
         events = self.client.events.get(event, [])
         futures = self.client.futures.get(event, [])
 
@@ -98,10 +138,24 @@ class State:
             self.loop.create_task(callback(*payload))
 
     async def parse_ready(self, data: Dict) -> None:
+        """
+        Parses the `READY` event. Creates a User then dispatches it afterwards.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         user = User(self, data["user"])
         self.dispatch("ready", user)
 
     async def parse_guild_create(self, data: Dict) -> None:
+        """
+        Parses `GUILD_CREATE` event. Creates a Guild then caches it, as well as dispatching it afterwards.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         guild = Guild(self, data)
 
         self.create_guild_channels(guild, data)
@@ -112,6 +166,13 @@ class State:
         self.dispatch("guild_create", guild)
 
     async def parse_message_create(self, data: Dict) -> None:
+        """
+        Parses `MESSAGE_CREATE` event. Creates a Message then caches it, as well as dispatching it afterwards.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         channel = self._channels.get(int(data["channel_id"]))
         message = Message(self, data, channel)  # type: ignore
 
@@ -119,6 +180,14 @@ class State:
         self.dispatch("message_create", message)
 
     async def parse_message_delete(self, data: Dict) -> None:
+        """
+        Parses `MESSAGE_DELETE` event. Retrieves the message from cache if possible.
+        Else it dispatches a `DeletedMessage`.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         deleted = DeletedMessage(data)
         message = self._messages.get(deleted.id)
 
@@ -130,6 +199,13 @@ class State:
         self.dispatch("message_delete", message)
 
     async def parse_message_update(self, data: Dict) -> None:
+        """
+        Parses `MESSAGE_UPDATE` event. Dispatches `before` and `after`.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         channel = self.get_channel(int(data["channel_id"]))
         if not channel:
             return
@@ -146,6 +222,13 @@ class State:
         self.dispatch("message_update", before, after)
 
     async def parse_channel_create(self, data: Dict) -> None:
+        """
+        Parses `CHANNEL_CREATE` event. Creates a Channel then caches it, as well as dispatching it afterwards.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         if guild_id := data.get("guild_id"):
             guild = self.get_guild(int(guild_id))
             channel = self.create_channel(data, guild)
@@ -156,6 +239,13 @@ class State:
         self.dispatch("channel_create", channel)
 
     async def parse_channel_update(self, data: Dict) -> None:
+        """
+        Parses `CHANNEL_UPDATE` event. Dispatches `before` and `after`.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         guild = self.get_guild(int(data["guild_id"]))
 
         before = self.get_channel(int(data["id"]))
@@ -165,24 +255,71 @@ class State:
         self.dispatch("channel_update", before, after)
 
     async def parse_channel_delete(self, data: Dict) -> None:
+        """
+        Parses `CHANNEL_DELETE` event. Dispatches the deleted channel.
+
+        Parameters:
+            data (Dict): The raw data.
+
+        """
         channel = self.get_channel(int(data["id"]))
         self._channels.pop(channel.id)  # type: ignore
 
         self.dispatch("channel_delete", channel)
 
     def get_message(self, message_id: int) -> Optional[Message]:
+        """
+        Grabs a message from the cache.
+
+        Parameters:
+            message_id (int): The ID of the message.
+
+        Returns:
+            The [lefi.Message][] insance corresponding to the ID if found.
+
+        """
         return self._messages.get(message_id)
 
     def get_user(self, user_id: int) -> Optional[User]:
+        """
+        Grabs a user from the cache.
+
+        Parameters:
+            user_id (int): The ID of the user.
+
+        Returns
+            The [lefi.User][] instance corresponding to the ID if found.
+
+        """
         return self._users.get(user_id)
 
     def add_user(self, data: Dict) -> User:
+        """
+        Creates a user then caches it.
+
+        Parameters:
+            data (Dict): The data of the user.
+
+        Returns:
+            The created [lefi.User][] instance.
+
+        """
         user = User(self, data)
 
         self._users[user.id] = user
         return user
 
     def get_guild(self, guild_id: int) -> Optional[Guild]:
+        """
+        Grabs a guild from the cache.
+
+        Parameters:
+            guild_id (int): The ID of the guild.
+
+        Returns
+            The [lefi.Guild][] instance corresponding to the ID if found.
+
+        """
         return self._guilds.get(guild_id)
 
     def get_channel(
@@ -190,18 +327,61 @@ class State:
     ) -> Optional[
         Union[TextChannel, DMChannel, VoiceChannel, CategoryChannel, Channel]
     ]:
+        """
+        Grabs a channel from the cache.
+
+        Parameters:
+            channel_id (int): The ID of the channel.
+
+        Returns
+            The [lefi.Channel][] instance corresponding to the ID if found.
+
+        """
         return self._channels.get(channel_id)
 
     def create_message(self, data: Dict, channel: Any) -> Message:
+        """
+        Creates a Message instance.
+
+        Parameters:
+            data (Dict): The data of the message.
+            channel (Any): The channel of the message.
+
+        Returns:
+            The created [lefi.Message][] instance.
+
+        """
         return Message(self, data, channel)
 
     def create_channel(
         self, data: Dict, *args
     ) -> Union[TextChannel, VoiceChannel, CategoryChannel, Channel]:
+        """
+        Creates a Channel instance.
+
+        Parameters:
+            data (Dict): The data of the channel.
+            *args (Any): Extra arguments to pass to the channels constructor.
+
+        Returns:
+            The created [lefi.Channel][] instance.
+
+        """
         cls = self.CHANNEL_MAPPING.get(int(data["type"]), Channel)
         return cls(self, data, *args)  # type: ignore
 
     def create_guild_channels(self, guild: Guild, data: Dict) -> Guild:
+        """
+        Creates the channels of a guild.
+
+        Parameters:
+            guild (lefi.Guild): The guild which to create the channels for.
+            data (Dict): The data of the channels.
+
+        Returns:
+            The [lefi.Guild][] instance passed in.
+
+        """
         channels = {
             int(payload["id"]): self.create_channel(payload, guild)
             for payload in data["channels"]
@@ -214,6 +394,17 @@ class State:
         return guild
 
     def create_guild_members(self, guild: Guild, data: Dict) -> Guild:
+        """
+        Creates the members of a guild.
+
+        Parameters:
+            guild (lefi.Guild): The guild which to create the channels for.
+            data (Dict): The data of the members.
+
+        Returns:
+            The [lefi.Guild][] instance passed in.
+
+        """
         members = {
             int(payload["user"]["id"]): Member(self, payload, guild)
             for payload in data["members"]
@@ -223,6 +414,17 @@ class State:
         return guild
 
     def create_guild_roles(self, guild: Guild, data: Dict) -> Guild:
+        """
+        Creates the roles of a guild.
+
+        Parameters:
+            guild (lefi.Guild): The guild which to create the channels for.
+            data (Dict): The data of the roles.
+
+        Returns:
+            The [lefi.Guild][] instance passed in.
+
+        """
         roles = {
             int(payload["id"]): Role(self, payload, guild) for payload in data["roles"]
         }
