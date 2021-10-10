4bd87d56996b1236c4772ecd21567fa20726a8e0
an-dyy <andy.development@protonmail.com>
2021-10-10 12:58:22 -0400

    docs: Added documentation for `member.py`

--- lefi/objects/member.py
+++ lefi/objects/member.py
@@ -15,6 +15,14 @@ __all__ = ("Member",)
 
 
 class Member(User):
+    """
+    Represents a member of a guild.
+
+    Attributes:
+        guild (lefi.Guild): The [lefi.Guild][] instance which the member belongs to.
+
+    """
+
     def __init__(self, state: State, data: Dict, guild: Guild):
         self._member = data
         self.guild = guild
@@ -23,18 +31,30 @@ class Member(User):
 
     @property
     def nick(self) -> Optional[str]:
+        """
+        The nickname of of member.
+        """
         return self._member.get("nick")
 
     @property
     def roles(self) -> List[Role]:
+        """
+        The roles of the member.
+        """
         return []
 
     @property
     def joined_at(self) -> datetime.datetime:
+        """
+        A [datetime.datetime][] instance representing when the member joined the guild.
+        """
         return datetime.datetime.fromisoformat(self._member["joined_at"])
 
     @property
     def premium_since(self) -> Optional[datetime.datetime]:
+        """
+        How long the member has been a premium.
+        """
         timestamp = self._member.get("premium_since")
         if timestamp is None:
             return None
@@ -43,8 +63,14 @@ class Member(User):
 
     @property
     def deaf(self) -> bool:
+        """
+        Whether or not the member is deafend.
+        """
         return self._member["deaf"]
 
     @property
     def mute(self) -> bool:
+        """
+        Whether or not the member is muted.
+        """
         return self._member["mute"]
