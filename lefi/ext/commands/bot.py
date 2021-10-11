2bcaccecb67babce5d128e8045dac9119f65e964
an-dyy <andy.development@protonmail.com>
2021-10-10 22:19:11 -0400

    feat: Started working on commands

---
+++ lefi/ext/commands/bot.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Dict, List, Callable, Union, Tuple
+
+import lefi
+
+from .context import Context
+from .parser import StringParser
+
+
+class Bot(lefi.Client):
+    def __init__(self, prefix: str, token: str, *args, **kwargs) -> None:
+        super().__init__(token, *args, **kwargs)
+        self.add_listener(self.parse_commands, "message_create")
+        self.add_listener(self.handle_command_error, "command_error")
+
+        self.checks: List[Callable[..., bool]] = []
+        self.commands: Dict = {}
+        self.prefix = prefix
+
+    async def execute(self, ctx) -> None:
+        ...
+
+    async def get_context(self, message: lefi.Message) -> Context:
+        invoker = await self.get_prefix(message)
+        return Context(
+            message, StringParser(message.content, await self.get_prefix(message)), self
+        )
+
+    async def get_prefix(self, message: lefi.Message) -> Union[Tuple[str], str]:
+        if callable(self.prefix):
+            return await self.prefix(message)
+
+        return self.prefix
+
+    async def parse_commands(self, message: lefi.Message) -> None:
+        ctx = await self.get_context(message)
+
+    async def handle_command_error(self) -> None:
+        ...
---
+++ lefi/ext/commands/context.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+__all__ = ("Context",)
+
+
+class Context:
+    if TYPE_CHECKING:
+        from .parser import StringParser
+        from lefi import Message
+        from .bot import Bot
+
+    def __init__(self, message: Message, parser: StringParser, bot: Bot) -> None:
+        self.message = message
+        self.parser = parser
+        self.bot = bot
