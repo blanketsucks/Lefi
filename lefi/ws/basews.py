a27af0f6a41a21c8cacd4d2a42908c4222673ff6
andy <andy.development@protonmail.com>
2021-11-01 18:25:48 -0400

    feat: Add `event` to `websocket_message`

--- lefi/ws/basews.py
+++ lefi/ws/basews.py
@@ -106,7 +106,7 @@ class BaseWebsocketClient:
         if parser := getattr(self.client._state, f"parse_{event.lower()}"):
             await parser(data)
 
-        self.client._state.dispatch("websocket_message", data)
+        self.client._state.dispatch("websocket_message", event.lower(), data)
 
     async def reconnect(self) -> None:
         """
