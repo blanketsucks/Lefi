e4647de06622bc8db0c9f1cae74fd99efea74aca
an-dyy <andy.development@protonmail.com>
2021-12-02 16:30:07 -0500

    feat: Added MemberConverter

--- lefi/exts/commands/core/converters.py
+++ lefi/exts/commands/core/converters.py
@@ -15,7 +15,7 @@ import inspect
 import re
 import sys
 
-from lefi import Object, User, utils
+from lefi import Object, User, Member, utils
 
 if TYPE_CHECKING:
     from .context import Context
@@ -42,7 +42,7 @@ class Converter(Generic[T_co], metaclass=ConverterMeta):
     MENTION_REGEX: ClassVar[re.Pattern] = re.compile(r"<(?:@(?:!|&)?|#)([0-9]{15,20})>$")
 
     @staticmethod
-    async def convert(ctx: Context, data: str) -> Optional[T_co]:
+    async def convert(ctx: Context, data: str) -> T_co:
         """Converts a string into the corresponding type.
 
         Parameters
@@ -84,7 +84,7 @@ class ObjectConverter(Converter[Object]):
 
         Returns
         -------
-        Optional[:class:`.Object`]
+        :class:`.Object`
             The created Object instance from the data given.
         """
         found = Converter.ID_REGEX.match(data) or Converter.MENTION_REGEX.match(data)
@@ -120,7 +120,7 @@ class UserConverter(Converter[User]):
 
         Returns
         -------
-        Optional[:class:`.User`]
+        :class:`.User`
             The User instance from the data given.
         """
         found = Converter.ID_REGEX.match(data) or Converter.MENTION_REGEX.match(data)
@@ -147,6 +147,63 @@ class UserConverter(Converter[User]):
             return user_
 
         raise TypeError(f"{data!r} cannot be converted to a User")
+
+
+class MemberConverter(Converter[Member]):
+    @staticmethod
+    async def convert(ctx: Context, data: str) -> Member:
+        """Converts the string given into a User.
+
+        Accepted arguments:
+        - id or mention
+        - username and discriminator
+        - username
+
+        Parameters
+        ----------
+        ctx: :class:`.Context`
+            The invocation context
+
+        data: :class:`str`
+            The data to convert into a :class:`.Member`
+
+        Raises
+        ------
+        :exc:`TypeError`
+            The data given couldn't be converted.
+
+        Returns
+        -------
+        :class:`.Member`
+            The Member instance from the data given.
+        """
+        found = Converter.ID_REGEX.match(data) or Converter.MENTION_REGEX.match(data)
+        guild = ctx.guild
+
+        if guild is None:
+            raise TypeError("Cannot convert {data!r} to a Member inside of DMChannel.")
+
+        if found is not None:
+            member_id = int(found.group(1))
+            member = guild.get_member(member_id) or await guild.fetch_member(member_id)
+
+            if member is None:
+                raise TypeError(f"{data!r} cannot be converted to a Member")
+
+            return member
+
+        data.removeprefix("@")
+        username: str = data[:-5]
+        discriminator: str = data[-4:]
+
+        if len(data) > 5 and data[-5] == "#" and discriminator.isdigit():
+            if member_ := utils.get(guild.members, username=username, discriminator=int(discriminator)):
+                return member_
+
+        if member_ := utils.get(guild.members, username=data) or utils.get(guild.members, nick=data):
+            return member_
+
+        raise TypeError(f"{data!r} cannot be converted to a Member")
 
 
 _CONVERTERS: Dict[str, Type[Converter]] = {}
