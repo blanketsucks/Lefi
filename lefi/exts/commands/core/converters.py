09ddb1e86ef4320683c58cbd6eb319107a98c5ec
an-dyy <andy.development@protonmail.com>
2021-12-02 13:32:44 -0500

    feat: Added UserConverter

--- lefi/exts/commands/core/converters.py
+++ lefi/exts/commands/core/converters.py
@@ -15,7 +15,7 @@ import inspect
 import re
 import sys
 
-from lefi import Snowflake, Object
+from lefi import Object, User, utils
 
 if TYPE_CHECKING:
     from .context import Context
@@ -35,8 +35,12 @@ class ConverterMeta(type):
 
 
 class Converter(Generic[T_co], metaclass=ConverterMeta):
-    __convert_type__: Type
-    """A base converter class."""
+    """A base converter class.
+    All converters should inherit this class.
+    """
+    __convert_type__: Type = None
+    ID_REGEX: ClassVar[re.Pattern] = re.compile(r"([0-9]{15,20})$")
+    MENTION_REGEX: ClassVar[re.Pattern] = re.compile(r"<(?:@(?:!|&)?|#)([0-9]{15,20})>$")
 
     @staticmethod
     async def convert(ctx: Context, data: str) -> Optional[T_co]:
@@ -59,12 +63,12 @@ class Converter(Generic[T_co], metaclass=ConverterMeta):
 
 
 class ObjectConverter(Converter[Object]):
-    ID_REGEX: ClassVar[re.Pattern] = re.compile(r"([0-9]{15,20})$")
-    MENTION_REGEX: ClassVar[re.Pattern] = re.compile(r"<(?:@(?:!|&)?|#)([0-9]{15,20})>$")
-
     @staticmethod
     async def convert(ctx: Context, data: str) -> Object:
         """Converts the string given into a Object.
+
+        Accepted arguments:
+        - id or mention
 
         Parameters
         ----------
@@ -74,17 +78,78 @@ class ObjectConverter(Converter[Object]):
         data: :class:`str`
             The data to convert into a :class:`.Object`
 
+        Raises
+        ------
+        :exc:`TypeError`
+            The data given couldn't be converted.
+
         Returns
         -------
         Optional[:class:`.Object`]
             The created Object instance from the data given.
         """
-        found = ObjectConverter.ID_REGEX.match(data) or ObjectConverter.MENTION_REGEX.match(data)
+        found = Converter.ID_REGEX.match(data) or Converter.MENTION_REGEX.match(data)
 
         if found is None:
             raise TypeError(f"{data!r} cannot be converted to Object")
-        
+
         return Object(id=int(found.group(1)))
+
+
+class UserConverter(Converter[User]):
+    @staticmethod
+    async def convert(ctx: Context, data: str) -> User:
+        """Converts the string given into a User.
+
+        Accepted arguments:
+        - id or mention
+        - username and discriminator
+        - username
+
+        Parameters
+        ----------
+        ctx: :class:`.Context`
+            The invocation context
+
+        data: :class:`str`
+            The data to convert into a :class:`.User`
+
+        Raises
+        ------
+        :exc:`TypeError`
+            The data given couldn't be converted.
+
+        Returns
+        -------
+        Optional[:class:`.User`]
+            The User instance from the data given.
+        """
+        found = Converter.ID_REGEX.match(data) or Converter.MENTION_REGEX.match(data)
+        bot = ctx.bot
+
+        if found is not None:
+            user_id = int(found.group(1))
+            user = bot.get_user(user_id) or await bot.fetch_user(user_id)
+
+            if user is None:
+                raise TypeError(f"{data!r} cannot be converted to a User")
+
+            return user
+
+        data.removeprefix("@")
+        username: str = data[:-5]
+        discriminator: str = data[-4:]
+
+        if len(data) > 5 and data[-5] == "#" and discriminator.isdigit():
+            if user := utils.get(
+                bot.users, username=username, discriminator=int(discriminator)
+            ):
+                return user
+
+        if user := utils.get(bot.users, username=data):
+            return user
+
+        raise TypeError(f"{data!r} cannot be converted to a User")
 
 
 _CONVERTERS: Dict[str, Type[Converter]] = {}
